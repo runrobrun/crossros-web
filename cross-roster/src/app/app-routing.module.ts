c36fdf44a45c1c646aa84d0f74edbade5ba47597
Rob Hammann <rhammann@vermeer.com>
2022-02-02 14:29:03 -0600

    adding about

--- cross-roster/src/app/app-routing.module.ts
+++ cross-roster/src/app/app-routing.module.ts
@@ -2,15 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './home/home.component';
+import {AboutComponent} from "./about/about.component";
 
 const routes: Routes = [
   {
     path: '',
-    component: HomeComponent,
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
   },
   {
     path: '**',
-    redirectTo: '/',
+    redirectTo: '/'
   }
 ]
 
